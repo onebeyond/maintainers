9cec82024c48b959dd5d4b321d904b1da3b68273
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-02-04 00:05:13 +0100

    feat: migrated to docusaurus
    - Used generator-fast-documentation to generate the project structure and migrate the docs as they are.
    - Full process transformation process inhttps://www.youtube.com/watch?v=Gpdw-oDIgBQ

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
-# admin
+# One Beyond Maintainers
 
-## About 
+One Beyond’s commitment to the open source community encourages an attitude of collaboration, transparency, and flexibility
 
-One Beyond’s commitment to the open source community encourages an attitude of **collaboration, transparency, and flexibility**; a philosophy that enables developers across the world to fulfil their potential and create ground-breaking software, as well as giving back to the open source community the same value that we receive from it.
+## Website
 
-This repository is used to coordinate the Open Source Activity and the communications with the community
+This website is built using [Docusaurus 2](https://docusaurus.io/) and [generator-fast-documentation](https://github.com/UlisesGascon/generator-fast-documentation)
 
-- [Meeting (including agenda, notes, recordings...)](/meetings)
-- [How to guides](/how-to)
-- [Governance](/governance)
+### Installation
 
+```
+$ npm i
+```
+
+### Local Development
+
+```
+$ npm start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ npm run build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Manage releases
+
+- Check [standard-version docs](https://github.com/conventional-changelog/standard-version)
+
+### Manage git Hooks
+
+- Check [Husky docs](https://github.com/typicode/husky)
---
+++ docusaurus.config.js
@@ -0,0 +1,62 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'One Beyond Maintainers',
+  tagline: 'One Beyond’s commitment to the open source community encourages an attitude of collaboration, transparency, and flexibility',
+  url: 'https://maintainers.opensource.one-beyond.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  onDuplicateRoutes: 'throw',
+  favicon: 'img/favicon.ico',
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          editUrl: undefined
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      })
+    ]
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'One Beyond Maintainers',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.png'
+          // srcDark: 'img/logo_dark.png'
+        },
+        items: [
+          {
+            href: 'https://github.com/onebeyond/maintainers',
+            label: 'Repository',
+            position: 'right'
+          }
+        ]
+      },
+      footer: {
+        style: 'dark',
+        copyright: 'Built with Docusaurus.'
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme
+      }
+    })
+}
+
+module.exports = config
