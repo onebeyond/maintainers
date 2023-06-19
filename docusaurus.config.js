c1676a81e7bc5987a4a4044ef89760530c845a9c
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-06-19 09:29:02 +0200

    chore: update dependencies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const { themes } = require('prism-react-renderer')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -53,8 +52,8 @@ const config = {
         copyright: 'Built with Docusaurus.'
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
+        theme: themes.github,
+        darkTheme: themes.dracula
       }
     })
 }
